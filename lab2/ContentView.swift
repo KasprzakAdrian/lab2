38e718c68c6688315d871ebcf0ae163d877eaea6
Patryk Balinski <patrykbalinski00@gmail.com>
2024-04-27 08:24:53 +0200

    Initial Commit

---
+++ lab2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab2
+//
+//  Created by student on 2024-04-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab2/lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  lab2App.swift
+//  lab2
+//
+//  Created by student on 2024-04-27.
+//
+
+import SwiftUI
+
+@main
+struct lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
